f2970c833cb9d655ad169618db0874df3f761a81
Gigabyte5671 <gigabyte5671@gmail.com>
2021-04-05 21:13:21 +1200

    Initial commit

---
+++ login/DDWCY3MP.js
@@ -0,0 +1,19 @@
+function F4UZG5F3(){
+  var GK9LXSCX = sha256($("#username")[0].value);
+  var MBHZE2KH = sha256($("#password")[0].value);
+  $.ajax({
+    dataType: "json",
+    url:"acc/" + GK9LXSCX + ".json",
+    success:function(OAMPKXZY){
+      if(OAMPKXZY.b == MBHZE2KH){
+        setCookie("arl", OAMPKXZY.c, 7);
+        window.location.href = "../admin/";
+      }else{
+        console.log("Wrong Creds");
+      }
+    },
+    error:function(xhr, status, err){
+      console.log("Error " + xhr.status);
+    }
+  });
+}
---
+++ status.js
@@ -0,0 +1,46 @@
+var statusJSON = {
+  "online_servers":{
+    "AUS Pub Server 1":{
+      "mode":"OOTB",
+      "type":"CTF",
+      "map":"Raindance",
+      "players":["Gigabyte5671","DaSexyChosenOne","Karuciel","Sacrificium","Gredwa","Pyrex","Gryoz"]
+    },
+    "G26's EU pub server 1":{
+      "mode":"GOTY",
+      "type":"CTF",
+      "map":"Katabatic",
+      "players":["Legend911","Frayz0r","Sparx","Agilent","Mikesters"]
+    },
+    "NA TDM":{
+      "mode":"OOTB",
+      "type":"TDM",
+      "map":"Katabatic Night",
+      "players":["IcedWinds","Mal","Potter","Griffon26","Zeke","Shaoliin"]
+    },
+    "Dodge's Arena Server":{
+      "mode":"GOTY",
+      "type":"Arena",
+      "map":"Lava Arena",
+      "players":[]
+    },
+    "Dodge's GOTY Server":{
+      "mode":"GOTY",
+      "type":"CTF",
+      "map":"Drydock",
+      "players":[]
+    },
+    "Dodge's Map Testing S":{
+      "mode":"GOTY",
+      "type":"CTF",
+      "map":"Test",
+      "players":[]
+    },
+    "G26's EU pub server 2":{
+      "mode":"GOTY",
+      "type":"CTF",
+      "map":"Ice Coaster",
+      "players":[]
+    }
+  }
+};
