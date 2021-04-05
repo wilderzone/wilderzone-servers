4ca6b9c68a34398f39889c6e92f33fdfb163acf2
Gigabyte5671 <gigabyte5671@gmail.com>
2021-04-06 11:51:12 +1200

    Added page alerts

--- login/DDWCY3MP.js
+++ login/DDWCY3MP.js
@@ -9,11 +9,21 @@ function F4UZG5F3(){
         setCookie("arl", OAMPKXZY.c, 7);
         window.location.href = "../admin/";
       }else{
-        console.log("Wrong Creds");
+        $(".alert").children(":nth-child(2)").html("Incorrect Credentials");
+        $(".alert").removeClass("hidden");
       }
     },
     error:function(xhr, status, err){
       console.log("Error " + xhr.status);
+      var alertMsg = "";
+      if(xhr.status == 404){
+        alertMsg = "Incorrect Credentials";
+      }else{
+        alertMsg = "Network Error: Please contact the site admin.";
+      }
+      $(".alert").children(":nth-child(2)").html(alertMsg);
+      $(".alert").addClass("alert_error");
+      $(".alert").removeClass("hidden");
     }
   });
 }
